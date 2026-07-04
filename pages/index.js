199a9c215488faba9eb95d51d4a4fa8dfb36fdbf
dreampulsemap <elsuilgen33@gmail.com>
2026-07-04 12:06:56 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { supabase } from '../lib/supabase'
 import dynamic from 'next/dynamic'
 import { supabase, auth } from '../lib/supabase'
 
