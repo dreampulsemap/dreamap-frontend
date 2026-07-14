a0b8f729d8809aec458eee7e5745113c1796c379
dreampulsemap <elsuilgen33@gmail.com>
2026-07-14 22:20:04 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import '../lib/i18n'  // i18n'i import et
+import Navbar from '../components/Navbar'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  
+  // Navbar'ın görünmesini istemediğimiz tam ekran veya özel rotalar
+  const hideNavbarPaths = ['/globe', '/auth/callback', '/analizetgulum']
+  const shouldHideNavbar = hideNavbarPaths.includes(router.pathname)
+
+  return (
+    <>
+      {!shouldHideNavbar && <Navbar />}
+      <Component {...pageProps} />
+    </>
+  )
 }
