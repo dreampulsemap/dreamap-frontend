2a285e1189b6d38db82bfbcc407ab3b1e84c2b17
dreampulsemap <elsuilgen33@gmail.com>
2026-07-15 17:57:11 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,26 @@
 import '@/styles/globals.css'
-import '../lib/i18n'  // i18n'i import et
-import Navbar from '../components/Navbar'
+import '@/lib/i18n'
+import Navbar from '@/components/Navbar'
+import BottomNav from '@/components/BottomNav'
 import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
   
-  // Navbar'ın görünmesini istemediğimiz tam ekran veya özel rotalar
+  // Tam ekran olan Küre veya hata sayfalarında menüleri gizle
   const hideNavbarPaths = ['/globe', '/auth/callback', '/analizetgulum']
   const shouldHideNavbar = hideNavbarPaths.includes(router.pathname)
 
   return (
     <>
       {!shouldHideNavbar && <Navbar />}
-      <Component {...pageProps} />
+      
+      {/* Mobilde BottomNav içeriğin üstüne binmesin diye pb-20 eklendi */}
+      <div className={!shouldHideNavbar ? "pb-20 md:pb-0" : ""}>
+        <Component {...pageProps} />
+      </div>
+
+      {!shouldHideNavbar && <BottomNav />}
     </>
   )
 }
