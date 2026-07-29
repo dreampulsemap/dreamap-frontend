05d1f578dd4fcb73b2945f152ef2c09d2a6f2cfc
dreampulsemap <elsuilgen33@gmail.com>
2026-07-29 17:32:22 +0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import '@/lib/i18n'
 import Navbar from '@/components/Navbar'
 import BottomNav from '@/components/BottomNav'
+import Sidebar from '@/components/Sidebar'
 import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
@@ -14,9 +15,11 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       {!shouldHideNavbar && <Navbar />}
+      {!shouldHideNavbar && <Sidebar />}
       
-      {/* Mobilde BottomNav içeriğin üstüne binmesin diye pb-20 eklendi */}
-      <div className={!shouldHideNavbar ? "pb-20 md:pb-0" : ""}>
+      {/* Mobilde BottomNav içeriğin üstüne binmesin diye pb-20, masaüstünde
+          Sidebar'ın altında kalmasın diye md:pl-64 eklendi */}
+      <div className={!shouldHideNavbar ? "pb-20 md:pb-0 md:pl-64" : ""}>
         <Component {...pageProps} />
       </div>
 
